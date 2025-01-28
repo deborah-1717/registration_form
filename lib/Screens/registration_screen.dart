09ac8f35115aa71008f9ce335c400f2d52770f87
deborah <deborah>
2025-01-28 12:13:51 +0100

    github.com/Deborah-1717

--- lib/Screens/registration_screen.dart
+++ lib/Screens/registration_screen.dart
@@ -1,25 +1,21 @@
+
+
 import 'package:flutter/material.dart';
 
-class RegistrationScreen extends StatefulWidget {
-  @override
+class  RegistrationScreen extends StatefulWidget{
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
-
-class _RegistrationScreenState extends State<RegistrationScreen> {
-  final TextEditingController firstnameController = TextEditingController();
-  final TextEditingController lastnameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  bool isRegistered = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Registration Form"),
-      ),
-       
-            
-      ),
-  
-  }
+class _RegistrationScreenState extends State<RegistrationScreen>{
+  final TextEditingController firstnameController= TextEditingController();
+  final  TextEditingController lastnameController= TextEditingController();
+  final  TextEditingController emailController= TextEditingController();
+  bool isRegistered=false;
+}
+@override  
+Widget build(BuildContext context){
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("Registration Form"),
+    ),
+  );
 }
