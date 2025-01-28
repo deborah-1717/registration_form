8700b1ee5556dd094d1a6de3eac6ca51b0a54e9e
deborah <deborah>
2025-01-28 13:47:40 +0100

    https://github.com/Deborah-1717

--- lib/Screens/registration_screen.dart
+++ lib/Screens/registration_screen.dart
@@ -6,16 +6,46 @@ class  RegistrationScreen extends StatefulWidget{
   _RegistrationScreenState createState() => _RegistrationScreenState();
 }
 class _RegistrationScreenState extends State<RegistrationScreen>{
-  final TextEditingController firstnameController= TextEditingController();
-  final  TextEditingController lastnameController= TextEditingController();
+  final TextEditingController firstNameController= TextEditingController();
+  final  TextEditingController lastNameController= TextEditingController();
   final  TextEditingController emailController= TextEditingController();
   bool isRegistered=false;
-}
-@override  
+
+
+  @override  
 Widget build(BuildContext context){
   return Scaffold(
     appBar: AppBar(
       title: Text("Registration Form"),
     ),
+    body: Padding
+    (padding:
+    EdgeInsets.all(20),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.
+      center ,
+      children: [
+        TextField(
+          controller: 
+          firstNameController,
+        ),
+                  TextField(
+                    controller:
+                    lastNameController,
+                  ),
+        TextField(
+          controller:emailController,
+            decoration: const
+           InputDecoration(
+            labelText: "Email Address"
+           ),
+        
+        
+        ),
+      ],
+    ),
+    ),
   );
 }
+}
+
